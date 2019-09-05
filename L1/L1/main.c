213199e6555185c99fe264da84c147dc67873e65
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-09-05 21:23:47 +0300

    + L1

--- L1/L1/main.c
+++ L1/L1/main.c
@@ -8,8 +8,71 @@
 
 #include <stdio.h>
 
+void task18(){
+    int i, n = 0;
+    double sum = 0;
+    
+    printf("\nPerforming task 18\nInput n:\n-> ");
+    scanf("%d", &n);
+    for (i = 1; i<=n; i++) {
+        sum = sum + 1.0/i;
+    }
+    
+    printf("Sum: %lf\n\n", sum);
+}
+
+void task26(){
+    int i, n = 0;
+    
+    printf("\nPerforming task 26\nInput n:\n-> ");
+    scanf("%d", &n);
+    
+    if (n > 0) {
+        printf("Natural dividers: 1");
+        for (i = 2; i<=n/2; i++) {
+            if (n % i == 0) {
+                printf(" %d", i);
+            }
+        }
+        printf(" %d", n);
+    } else {
+        printf("n is not a natural number");
+    }
+    
+    printf("\n\n");
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int x, rdyToExit = 0, infoIsShown = 0;
+    
+    while (rdyToExit != 1) {
+        if (infoIsShown == 0) {
+            printf("Choose task:\n1. 18\n2. 26\n3. Exit\n");
+            infoIsShown = 1;
+        }
+        
+        printf("-> ");
+        scanf("%d", &x);
+        
+        switch (x) {
+            case 1:
+                task18();
+                infoIsShown = 0;
+                break;
+            case 2:
+                task26();
+                infoIsShown = 0;
+                break;
+            case 3:
+                rdyToExit = 1;
+                break;
+            default:
+                printf("Invalid input\n");
+                break;
+        }
+        
+        
+        
+    }
     return 0;
 }
