f56f079622fc755bffd5547f38db59d97f25c4e7
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-09-26 10:04:50 +0300

    + L4

---
+++ L4/L4_1/L4_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  L4_1
+//
+//  Created by Михаил Ковалевский on 26/09/2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
