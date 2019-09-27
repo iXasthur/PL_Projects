7b288e85ca41f34e3155352eb9c01642e9b3d38f
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-09-27 11:38:39 +0300

    Update main.c

--- L4/L4_1/L4_1/main.c
+++ L4/L4_1/L4_1/main.c
@@ -7,9 +7,81 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define WORDLENGTH 20
+
+
+struct translation {
+    char initial[WORDLENGTH];
+    char result[WORDLENGTH];
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    const int dictionaryLength = 5;
+    const struct translation dictionary[dictionaryLength] = {{"this","это"},{"table","стол"},{"is",""},{"a",""},{"very","очень"}};
+    char str[256];
+    char resultStr[256];
+    scanf("%s",str);
+    
+    char words[50][WORDLENGTH];
+    int i = 0;
+    int wordCount = 0;
+    
+    while (str[i] != '\0') {
+        int wordPos = 0;
+        char w[WORDLENGTH] = "";
+        
+        while (str[i] != '_' && str[i] != '\0'){
+            w[wordPos] = str[i];
+            wordPos++;
+            i++;
+        }
+        
+        if (strcmp(w, "") != 0) {
+            strcpy(words[wordCount], w);
+            wordCount++;
+        }
+        i++;
+    }
+    
+//    for (i = 0; i<wordCount; i++) {
+//        printf("%s\n", words[i]);
+//    }
+    
+    int j = 0;
+    int foundTranslation = 0;
+    for (i = 0; i<wordCount; i++) {
+        foundTranslation = 0;
+        j = 0;
+
+        while (!foundTranslation && j<dictionaryLength) {
+            if (strcmp(words[i], dictionary[j].initial) == 0){
+                foundTranslation = 1;
+                
+                if (strcmp(resultStr, "") == 0 && strcmp(dictionary[j].result, "") != 0) {
+                    strcat(resultStr, dictionary[j].result);
+                } else if (strcmp(dictionary[j].result, "") != 0){
+                    
+                    strcat(resultStr, "_");
+                    strcat(resultStr, dictionary[j].result);
+                }
+            }
+            j++;
+        }
+        
+        if (!foundTranslation) {
+            if (strcmp(resultStr, "") == 0 && strcmp(words[i], "") != 0) {
+                strcat(resultStr, words[i]);
+            } else if (strcmp(words[i], "") != 0){
+                strcat(resultStr, "_");
+                strcat(resultStr, words[i]);
+            }
+        }
+    }
+    
+    printf("%s\n",resultStr);
+    
     return 0;
 }
