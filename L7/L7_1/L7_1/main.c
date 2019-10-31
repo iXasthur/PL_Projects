a416593dbf866b2dbe945a2e8cd3957f7e2b677b
Mikhail Kavaleuski <38611299+iXasthur@users.noreply.github.com>
2019-10-31 19:51:04 +0300

    +L7_1 prj

---
+++ L7/L7_1/L7_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  L7_1
+//
+//  Created by Михаил Ковалевский on 24.10.2019.
+//  Copyright © 2019 Mikhail Kavaleuski. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
